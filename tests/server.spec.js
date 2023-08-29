86dd9a3317a49e859d49633b224ada46f3fb1e44
Kelsie Smith <kelsies@missionreadyhq.com>
2023-08-30 11:58:06 +1200

    updated test file

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,23 +1,16 @@
 const request = require("supertest");
-const express = require("express");
+const { app } = require("../server-setup");
 const fs = require("fs");
-const app = express();
 
-// Import the routes module and server setup module
-const routes = require("../routes");
-const { app: serverApp } = require("../server-setup");
+describe("API Tests", () => {
+  it("should respond with 200 OK when POSTing to /api", async () => {
+    const imageBuffer = fs.readFileSync("./images/testcar1.jpg");
 
-// Mount the routes on the app
-app.use("/", routes);
+    const response = await request(app)
+      .post("/api")
+      .set("Content-Type", "image/jpeg")
+      .send(imageBuffer);
 
-describe("POST /api", () => {
-  it("should respond with high probability predictions", async () => {
-    const imageData = fs.readFileSync("./images/testcar1.jpg");
-
-    // Make a request to your Express app using supertest
-    const response = await request(serverApp).post("/api").send(imageData);
-
-    expect(response.statusCode).toBe(200);
-    // Add more assertions to check the response data as needed
+    expect(response.status).toEqual(200);
   });
 });
