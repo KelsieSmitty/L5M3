ae966de6c0772a8b6cb97041c77fd32bdbd1ca08
Kelsie Smith <kelsies@missionreadyhq.com>
2023-08-31 15:39:32 +1200

    tetsing file updated

--- test/servertest.spec.js
+++ test/servertest.spec.js
@@ -5,11 +5,11 @@ const app = require("../server");
 
 describe("Image API Tests", () => {
   it("should respond with a 200 status code after sending an image", async () => {
-    const imagePath = path.join(__dirname, "images", "car.jpg");
+    const imagePath = path.join(__dirname, "images", "pic1.jpg");
     const imageBuffer = fs.readFileSync(imagePath);
 
     const response = await request(app)
-      .post("/api/")
+      .post("/api")
       .send(imageBuffer)
       .set("Content-Type", "image/jpeg");
 
